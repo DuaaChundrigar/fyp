89324313c8066f028d27e2e85ea2ef71af886285
DuaaChundrigar <abdullahduaa85@gmail.com>
2022-04-18 00:38:13 +0500

    added Request vue page but unable to redirect it

--- resources/js/Pages/User/Request.vue
+++ resources/js/Pages/User/Request.vue
@@ -1,5 +1,7 @@
 <template>
-    <nav class="bg-gray-800">
+
+<nav class="bg-gray-800">
+        
         <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
                 <div
@@ -12,23 +14,23 @@
                             alt="Logo"
                         />
                     </div>
-                    
-                    <div class="ml-4 font-serif text-4xl font-bold text-gray-700 underline">
-        <button
-            type="button"
-            class="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded"
-            id="rfb"
-        >
-            <Link href="#" class=""> Request for Book </Link>
-        </button>
-        <button
-            type="button"
-            class="px-3 py-2 ml-2 text-sm font-medium text-white bg-gray-900 rounded"
-            id="lor"
-        >
-            <Link href="#" class=""> List of Request </Link>
-        </button>
-    </div>
+
+                    <div class="m-10 font-serif text-4xl font-bold text-gray-700 underline">
+                        <button
+                            type="button"
+                            class="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded"
+                            id="rfb"
+                        >
+                            <Link href="/Dashboard" class=""> Request for Book </Link>
+                        </button>
+                        <button
+                            type="button"
+                            class="px-3 py-2 ml-2 text-sm font-medium text-white bg-gray-900 rounded"
+                            id="lor"
+                        >
+                            <Link href="/Request.vue" class=""> List of Request </Link>
+                        </button>
+                    </div>
                     
                 </div>
                 <div
@@ -43,48 +45,15 @@
                             <Link href="/logout" class="">Logout</Link>
                         </button>
                     </div>
+                    
                 </div>
             </div>
         </div>
     </nav>
 
-   
 
-    <div class="flex justify-center mt-5">
-        <div class="mb-3 xl:w-96">
-            <div
-                class="relative flex flex-wrap items-stretch w-full mb-4 input-group"
-            >
-                <input
-                    type="search"
-                    class="form-control relative flex-auto min-w-0 block px-3 py-1.5 text-base font-normal text-gray-700 bg-white border border-solid border-gray-300 rounded focus:bg-white focus:border-blue-600"
-                    placeholder="Search for Book"
-                    aria-label="Search"
-                />
-                <button
-                    class="btn px-6 py-2.5 bg-blue-600 text-white rounded shadow-md hover:bg-blue-700 active:bg-blue-800"
-                    type="button"
-                    id=""
-                >
-                    <svg
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fas"
-                        data-icon="search"
-                        class="w-4"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-                        ></path>
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
+
+
 
     <!-- Table  -->
 
@@ -140,6 +109,7 @@
                             </tr>
                         </thead>
                         <tbody>
+
                             <tr
                                 v-for="book in books"
                                 :key="book.id"
@@ -148,42 +118,42 @@
                                 <td
                                     class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap"
                                 >
-                                    {{ book.book_no }}
+                                {{ books.book_no }}
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap"
                                 >
-                                    {{ book.book_name }}
+                                    {{books.name}}
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap"
                                 >
-                                    {{ book.subject }}
+                                    {{books.subject}}
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap"
                                 >
-                                    {{ book.author }}
+                                    {{books.author}}
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap"
                                 >
-                                    {{ book.publisher }}
+                                    {{books.publisher}}
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap"
                                 >
-                                    {{ book.copies }}
+                                    {{books.copies}}
                                 </td>
                                 <td
                                     class="px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap"
                                 >
                                     <button
                                         type="button"
-                                        class="px-3 py-2 text-sm font-medium text-white bg-blue-600 rounded-md"
-                                        @click="request(book.id)"
+                                        class="px-3 py-2 text-sm font-medium text-white bg-red-600 rounded-md"
+                                        id="login"
                                     >
-                                        Request Submit
+                                        Pending
                                     </button>
                                 </td>
                             </tr>
@@ -193,41 +163,8 @@
             </div>
         </div>
     </div>
+
+
+
 </template>
 
-<script>
-import { Link } from "@inertiajs/inertia-vue3";
-import axios from "axios";
-export default {
-    components: {
-        Link,
-    },
-
-    props: {
-        user: {
-            type: Object,
-            default: {},
-        },
-        books: {
-            type: Array,
-            default: [],
-        },
-    },
-    mounted() {},
-
-    methods: {
-        request(book_id) {
-            axios
-                .post("/book/request", {
-                    book_id: book_id,
-                })
-                .then((response) => {
-                    alert(response.data);
-                })
-                .catch((error) => {
-                    alert(error.response.data);
-                });
-        },
-    },
-};
-</script>
